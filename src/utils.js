c61474627ad414c4ae6ab25aa41fda0a51fe9e28
koala-lava <15828770+koala-lava@users.noreply.github.com>
2020-01-16 23:51:41 +0700

    apply standard --fix

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,4 @@
-export function calculateWinner(squares) {
+export function calculateWinner (squares) {
   const lines = [
     [0, 1, 2],
     [3, 4, 5],
@@ -7,35 +7,35 @@ export function calculateWinner(squares) {
     [1, 4, 7],
     [2, 5, 8],
     [0, 4, 8],
-    [2, 4, 6],
-  ];
+    [2, 4, 6]
+  ]
   for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
+    const [a, b, c] = lines[i]
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
       return {
-        'winner': squares[a],
-        'winningLine': lines[i]
-      };
+        winner: squares[a],
+        winningLine: lines[i]
+      }
     }
   }
 
-  return {};
+  return {}
 }
 
-export function calculateDraw(squares) {
-  const empty = squares.indexOf(null);
-  //'empty>-2' allows to pass '0 index' as truthy value, not found -1 also ok 
+export function calculateDraw (squares) {
+  const empty = squares.indexOf(null)
+  // 'empty>-2' allows to pass '0 index' as truthy value, not found -1 also ok
   if (empty > -2 && squares.indexOf(null, empty + 1) === -1) {
-    const test = squares.slice();
-    let draw;
-    test[empty] = 'X';
+    const test = squares.slice()
+    let draw
+    test[empty] = 'X'
     if (!calculateWinner(test).winner) {
-      draw = true;
+      draw = true
     }
     if (draw) {
-      return { 'draw': true };
+      return { draw: true }
     }
   }
 
-  return {};
+  return {}
 }
